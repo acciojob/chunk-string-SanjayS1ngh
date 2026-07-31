862601af7089c902fb5ff25ccdcd5f15d9b9f37d
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-07-31 23:36:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 function stringChop(str, size) {
   // your code here
-	let n=Math.floor((str.length)/size);
-	let m=(str.length)%size;
-	let s="";
-	let k=size;
 	let ans=[];
-	for(let i=0;i<str.length-m;i++){
-		s+=str[i];
-		k--;
-		if(k==0){
-			ans.push(s);
-			s="";
-			k=size;
-		}
+	for(let i=0;i<str.length;i+size){
+		ans.push(str.slice(i,i+=size));
 	}
-	s="";
-	for(let i=str.length-m;i<str.length;i++){
-		s+=str[i];
-	}
-	ans.push(s);
 	return ans;
 }
 
